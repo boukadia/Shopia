0f18af55413964ebac73f73a2d281ae5ddb84694
boukadia <mustapha.boukadia97@gmail.com>
2025-12-24 11:46:24 +0100

    terminer la structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProduitsModule } from './produits/produits.module';
+import { CommandesModule } from './commandes/commandes.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProduitsModule, CommandesModule, InventoryModule, AuthModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
